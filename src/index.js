e11694806aef0f5487a3a22569fe2cf6088515bf
Christoph Werner <christoph+github@codepunkt.de>
2017-06-13 08:46:36 +0200

    Use prettier to auto-format

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import jsonpatch from 'fast-json-patch'
 import { decamelize, pascalize } from 'humps'
 import { dropRightWhile, each, map, merge, omit } from 'lodash'
 
-export const RollbackError = function (message, extra) {
+export const RollbackError = function(message, extra) {
   Error.captureStackTrace(this, this.constructor)
   this.name = 'RollbackError'
   this.message = message
@@ -13,11 +13,11 @@ export const RollbackError = function (message, extra) {
 
 require('util').inherits(RollbackError, Error)
 
-const createPatchModel = (options) => {
+const createPatchModel = options => {
   const def = {
     date: { type: Date, required: true, default: Date.now },
     ops: { type: [], required: true },
-    ref: { type: options._idType, required: true, index: true }
+    ref: { type: options._idType, required: true, index: true },
   }
 
   each(options.includes, (type, name) => {
@@ -36,15 +36,15 @@ const createPatchModel = (options) => {
 const defaultOptions = {
   includes: {},
   removePatches: true,
-  transforms: [ pascalize, decamelize ]
+  transforms: [pascalize, decamelize],
 }
 
 // used to convert bson to json - especially ObjectID references need
 // to be converted to hex strings so that the jsonpatch `compare` method
 // works correctly
-const toJSON = (obj) => JSON.parse(JSON.stringify(obj))
+const toJSON = obj => JSON.parse(JSON.stringify(obj))
 
-export default function (schema, opts) {
+export default function(schema, opts) {
   const options = merge({}, defaultOptions, opts)
 
   // get _id type from schema
@@ -58,7 +58,7 @@ export default function (schema, opts) {
 
   // used to compare instance data snapshots. depopulates instance,
   // removes version key and object id
-  schema.methods.data = function () {
+  schema.methods.data = function() {
     return this.toObject({
       depopulate: true,
       versionKey: false,
@@ -69,37 +69,39 @@ export default function (schema, opts) {
           delete ret[schema.options.timestamps.createdAt || 'createdAt']
           delete ret[schema.options.timestamps.updatedAt || 'updatedAt']
         }
-      }
+      },
     })
   }
 
-  // roll the document back to the state of a given patch id
-  schema.methods.rollback = function (patchId, data) {
-    return this.patches.find({ ref: this.id }).sort({ date: 1 }).exec()
-      .then((patches) => new Promise((resolve, reject) => {
-        // patch doesn't exist
-        if (!~map(patches, 'id').indexOf(patchId)) {
-          return reject(new RollbackError('patch doesn\'t exist'))
-        }
+  // roll the document back to the state of a given patch id()
+  schema.methods.rollback = function(patchId, data) {
+    return this.patches.find({ ref: this.id }).sort({ date: 1 }).exec().then(
+      patches =>
+        new Promise((resolve, reject) => {
+          // patch doesn't exist
+          if (!~map(patches, 'id').indexOf(patchId)) {
+            return reject(new RollbackError("patch doesn't exist"))
+          }
 
-        // get all patches that should be applied
-        const apply = dropRightWhile(patches, (patch) => patch.id !== patchId)
+          // get all patches that should be applied
+          const apply = dropRightWhile(patches, patch => patch.id !== patchId)
 
-        // if the patches that are going to be applied are all existing patches,
-        // the rollback attempts to rollback to the latest patch
-        if (patches.length === apply.length) {
-          return reject(new RollbackError('rollback to latest patch'))
-        }
+          // if the patches that are going to be applied are all existing patches,
+          // the rollback attempts to rollback to the latest patch
+          if (patches.length === apply.length) {
+            return reject(new RollbackError('rollback to latest patch'))
+          }
 
-        // apply patches to `state`
-        const state = {}
-        apply.forEach((patch) => {
-          jsonpatch.apply(state, patch.ops, true)
+          // apply patches to `state`
+          const state = {}
+          apply.forEach(patch => {
+            jsonpatch.apply(state, patch.ops, true)
+          })
+
+          // save new state and resolve with the resulting document
+          this.set(merge(data, state)).save().then(resolve).catch(reject)
         })
-
-        // save new state and resolve with the resulting document
-        this.set(merge(data, state)).save().then(resolve).catch(reject)
-      }))
+    )
   }
 
   // create patch model, enable static model access via `Patches` and
@@ -110,7 +112,7 @@ export default function (schema, opts) {
 
   // after a document is initialized or saved, fresh snapshots of the
   // documents data are created
-  const snapshot = function () {
+  const snapshot = function() {
     this._original = toJSON(this.data())
   }
   schema.post('init', snapshot)
@@ -118,24 +120,29 @@ export default function (schema, opts) {
 
   // when a document is removed and `removePatches` is not set to false ,
   // all patch documents from the associated patch collection are also removed
-  schema.pre('remove', function (next) {
+  schema.pre('remove', function(next) {
     if (!options.removePatches) {
       return next()
     }
 
     const { _id: ref } = this
-    this.patches.find({ ref })
-      .then((patches) => join(patches.map((patch) => patch.remove())))
-      .then(next).catch(next)
+    this.patches
+      .find({ ref })
+      .then(patches => join(patches.map(patch => patch.remove())))
+      .then(next)
+      .catch(next)
   })
 
   // when a document is saved, the json patch that reflects the changes is
   // computed. if the patch consists of one or more operations (meaning the
   // document has changed), a new patch document reflecting the changes is
   // added to the associated patch collection
-  schema.pre('save', function (next) {
+  schema.pre('save', function(next) {
     const { _id: ref } = this
-    const ops = jsonpatch.compare(this.isNew ? {} : this._original, toJSON(this.data()))
+    const ops = jsonpatch.compare(
+      this.isNew ? {} : this._original,
+      toJSON(this.data())
+    )
 
     // don't save a patch when there are no changes to save
     if (!ops.length) {
